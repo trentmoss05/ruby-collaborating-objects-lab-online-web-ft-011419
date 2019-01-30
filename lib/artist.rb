d29d831857c9cb557b691c471e6d21ec4d98eacb
Trent Moss <trentmoss05@gmail.com>
2019-01-30 06:21:55 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    artist = @@all.find do |artist|
+      name == artist.name
+    end
+    artist || Artist.new(name)
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+
+end
